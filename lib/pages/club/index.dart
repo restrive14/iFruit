e656dddea51f033710e618659d631f8818eab78e
yang <restrive_14@163.com>
2026-08-04 14:37:52 +0800

    feat: 事务所页面空状态

--- lib/pages/club/index.dart
+++ lib/pages/club/index.dart
@@ -2,12 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:ifruit/widgets/bottomBar.dart';
 import 'package:ifruit/widgets/topStatusBar.dart';
 
-class _FriendItem {
-  final String name;
-  final IconData? icon;
-  const _FriendItem({required this.name, this.icon});
-}
-
 class ClubPage extends StatefulWidget {
   const ClubPage({super.key});
 
@@ -17,37 +11,45 @@ class ClubPage extends StatefulWidget {
 
 class _ClubPageState extends State<ClubPage> {
   // 选中索引
-  int _selectedIndex = 1;
-  // 图标列表
-  static const List<_FriendItem> _friendList = [
-    _FriendItem(name: '好友1', icon: Icons.person),
-    _FriendItem(name: '好友1', icon: Icons.person),
-    _FriendItem(name: '好友1', icon: Icons.person),
-  ];
+  final int _selectedIndex = 1;
 
   // 点击图标选中
-  void _onTapSelectIcon(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
+  void _onTapSelectIcon(int index) {}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: TopStatusBar(title: '保镖事务所'),
-      body: ListView.builder(
-        itemCount: _friendList.length,
-        itemBuilder: (context, index) {
-          final friend = _friendList[index];
-          return ListTile(
-            leading: Icon(friend.icon),
-            title: Text(friend.name),
-            onTap: () => _onTapSelectIcon(index),
-          );
-        },
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(color: Colors.black),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 209, 133, 108),
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('没有邀请', style: TextStyle(fontSize: 25)),
+                  Text('可以进行', style: TextStyle(fontSize: 20)),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
-      bottomNavigationBar: const BottomBar(),
+      bottomNavigationBar: BottomBar(
+        showDel: true,
+        centerIcon: Icon(Icons.check_rounded, color: Colors.green, size: 50),
+        onTapPlus: () {},
+      ),
     );
   }
 }
